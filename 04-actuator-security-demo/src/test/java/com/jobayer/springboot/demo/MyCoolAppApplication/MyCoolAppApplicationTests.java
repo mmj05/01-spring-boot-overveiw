fb3ef451dafc3ccd0552e625aee752429b16fe51
Muhaimin Jobayer <mmj0993@gmail.com>
2023-08-08 13:01:15 -0500

    change properties

---
+++ 04-actuator-security-demo/src/test/java/com/jobayer/springboot/demo/MyCoolAppApplication/MyCoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobayer.springboot.demo.MyCoolAppApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
